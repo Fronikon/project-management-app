3d6dbd9df1bdd0c375957c66b9066ffdcdf5c104
Daniil Sharenkov <Milkiy174@gmail.com>
2022-11-09 13:28:08 +0300

    feat: add header layout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,35 @@
 import React, { FC } from 'react';
+import styles from './Header.module.css';
 
 const Header: FC = () => {
-  return <header>Header</header>;
+  return (
+    <header>
+      <div className={styles.leftBlock}>
+        <h1 className={styles.heading}>Doska</h1>
+        <div className={styles.home}>
+          <img src="" alt="" className={styles.homeImage} />
+          <p className={styles.homeText}>Home</p>
+        </div>
+      </div>
+      <div className={styles.rightBlock}>
+        <div className={styles.language}>
+          <button className={styles.languageSwitch}></button>
+          <div className={styles.languageText}>
+            <p className={styles.languageRus}>Русский</p>
+            <p className={styles.languageEng}>English</p>
+          </div>
+        </div>
+        <div className={styles.signIn}>
+          <img src="" alt="" className={styles.signInImage} />
+          <p className={styles.signInText}>Sign In</p>
+        </div>
+        <div className={styles.signUp}>
+          <img src="" alt="" className={styles.signUpImage} />
+          <p className={styles.signUpText}>Sign Up</p>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
