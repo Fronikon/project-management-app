53c069e3a3cc37fbd0948a78e915e00dd695a006
Daniil Sharenkov <Milkiy174@gmail.com>
2022-11-10 10:32:44 +0300

    feat: add welcome page layout

---
+++ src/components/Main/components/Welcome/Team/Team.tsx
@@ -0,0 +1,52 @@
+import React, { FC } from 'react';
+import { TeamListInterface } from '../../../../../types/interfaces';
+import styles from './Team.module.css';
+import Andrey from '../../../../../assets/Andrey.png';
+import Dmitriy from '../../../../../assets/Dmitriy.png';
+import Daniil from '../../../../../assets/Daniil.png';
+
+const Team: FC = () => {
+  const teamList: TeamListInterface[] = [
+    {
+      img: Andrey,
+      color: '#EABFFF',
+      engName: 'Andrey Lavrenov',
+      engPosition: 'Developer',
+      rusName: 'Андрей Лаврёнов',
+      rusPosition: 'Разработчик',
+      key: 1,
+    },
+    {
+      img: Dmitriy,
+      color: '#B1D0FF',
+      engName: 'Dmitry Beresnev',
+      engPosition: 'Team-lead',
+      rusName: 'Дмитрий Береснев',
+      rusPosition: 'Лидер команды',
+      key: 2,
+    },
+    {
+      img: Daniil,
+      color: '#93E396',
+      engName: 'Daniil Sharenkov',
+      engPosition: 'Developer',
+      rusName: 'Даниил Шаренков',
+      rusPosition: 'Разработчик',
+      key: 3,
+    },
+  ];
+
+  return (
+    <>
+      {teamList.map((person) => (
+        <div className={styles.person} key={person.key} style={{ backgroundColor: person.color }}>
+          <img src={person.img} alt={person.engName} />
+          <h2 className={styles.name}>{person.engName}</h2>
+          <p className={styles.position}>{person.engPosition}</p>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default Team;
--- src/components/Main/components/Welcome/Welcome.tsx
+++ src/components/Main/components/Welcome/Welcome.tsx
@@ -1,7 +1,23 @@
 import React, { FC } from 'react';
+import Team from './Team/Team';
+import styles from './Welcome.module.css';
 
 const Welcome: FC = () => {
-  return <div>Welcome</div>;
+  return (
+    <div className={styles.mainWrapper}>
+      <div className={styles.textWrapper}>
+        <p className={styles.text}>
+          Everyone can handle our application, just register and get started
+        </p>
+      </div>
+      <div className={styles.teamWrapper}>
+        <div className={styles.team}>
+          <Team />
+        </div>
+        <p className={styles.teamText}>Our Team</p>
+      </div>
+    </div>
+  );
 };
 
 export default Welcome;
