c8cb26a7b2e7ca580a072c1b0c6c58eac1eac678
Dmitry Beresnev <dmitrberesnev@gmail.com>
2022-11-13 14:40:47 +0300

    feat: add buttons boards

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,18 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import styles from './Header.module.css';
 import { switchEng, switchRu } from '../../store/reducers/languageReducer';
 import { useAppDispatch, useAppSelector } from '../../hooks/reduxHooks';
 import textData from '../../data/textData';
 import home from '../../assets/img/icons/home_image.jpg';
+import boards from '../../assets/img/icons/boards-logo.png';
+import addBoard from '../../assets/img/icons/add-board-logo.png';
 import signIn from '../../assets/img/icons/sing_in.jpg';
 import signUp from '../../assets/img/icons/sign_up.jpg';
 import edit from '../../assets/img/icons/edit.png';
 import exit from '../../assets/img/icons/exit.png';
-import { NavLink } from 'react-router-dom';
+import CreateBoardForm from './../../componentsUtils/forms/CreateBoardForm/CreateBoardForm';
+import Modal from './../../componentsUtils/Modal/Modal';
 
 const Header: FC = () => {
   const language = useAppSelector((store) => store.language.value);
@@ -23,14 +27,45 @@ const Header: FC = () => {
     }
   };
 
+  const [isOpenCreateBoardModal, setIsOpenCreateBoardModal] = useState(false);
+
+  const closeModal = () => {
+    setIsOpenCreateBoardModal(false);
+  };
+
+  const openModal = () => {
+    setIsOpenCreateBoardModal(true);
+  };
+
+  const renderCreateBoardModal = () => {
+    if (isOpenCreateBoardModal) {
+      return (
+        <Modal closeModal={closeModal}>
+          <CreateBoardForm closeModal={closeModal} />
+        </Modal>
+      );
+    }
+  };
+
   return (
     <header className={styles.headerWrapper}>
       <div className={styles.leftBlock}>
         <h1 className={styles.heading}>Doska</h1>
-        <NavLink className={styles.home} to="/" end>
-          <img src={home} alt="home" className={styles.homeImage} />
-          <p className={styles.homeText}>{textData.header.home[language]}</p>
-        </NavLink>
+        <div className={styles.buttons}>
+          <NavLink className={styles.home} to={'/'}>
+            <img src={home} alt="home" className={styles.homeImg} />
+            <p className={styles.homeText}>Home</p>
+          </NavLink>
+          <NavLink className={styles.board} to={'boards'}>
+            <img src={boards} alt="boards" className={styles.boardsImg} />
+            <p className={styles.boardText}>Boards</p>
+          </NavLink>
+          <button onClick={openModal} className={styles.addBoard}>
+            <img src={addBoard} alt="add board" className={styles.addBoardImg} />
+            <p className={styles.addBoardText}>Add board</p>
+          </button>
+        </div>
+        {renderCreateBoardModal()}
       </div>
       <div className={styles.rightBlock}>
         <div className={styles.language}>
