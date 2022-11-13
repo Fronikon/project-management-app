20ffed5f9e0041abf53a6516c8e5490a075cda3c
Andrey Lavryonov <kedr_club@mail.ru>
2022-11-13 09:29:38 +0300

    feat: add pageNotFound page

--- src/components/Main/components/PageNotFound/PageNotFound.tsx
+++ src/components/Main/components/PageNotFound/PageNotFound.tsx
@@ -1,7 +1,20 @@
 import React, { FC } from 'react';
+import styles from '../../../../components/Main/components/PageNotFound/PageNotFound.module.css';
+import fone from '../../../../assets/404.png';
 
 const PageNotFound: FC = () => {
-  return <div>PageNotFound</div>;
+  const title = {
+    ru: 'К сожалению такой страницы не существует',
+    en: 'Sorry, there is no such page',
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <span className={styles.letterD}>D</span>
+      <h2 className={styles.title}>{title.en}</h2>
+      <img className={styles.image} src={fone} alt="Error 404" />
+    </div>
+  );
 };
 
 export default PageNotFound;
