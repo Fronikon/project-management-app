7ed8c25b4a55013d5ba4e6a3413b7f15bca142c9
Dmitry Beresnev <dmitrberesnev@gmail.com>
2022-11-15 22:56:57 +0300

    feat: create button add board in boards page

--- src/components/Main/components/Boards/Boards.tsx
+++ src/components/Main/components/Boards/Boards.tsx
@@ -1,7 +1,42 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import CreateBoardForm from '../../../../componentsUtils/forms/CreateBoardForm/CreateBoardForm';
+import Modal from '../../../../componentsUtils/Modal/Modal';
+import styles from './Boards.module.css';
 
 const Boards: FC = () => {
-  return <div>Boards</div>;
+  const [isOpenCreateBoardModal, setIsOpenCreateBoardModal] = useState(false);
+
+  const closeModal = () => {
+    setIsOpenCreateBoardModal(false);
+  };
+
+  const openModal = () => {
+    setIsOpenCreateBoardModal(true);
+  };
+
+  const renderCreateBoardModal = () => {
+    if (isOpenCreateBoardModal) {
+      return (
+        <Modal closeModal={closeModal}>
+          <CreateBoardForm closeModal={closeModal} />
+        </Modal>
+      );
+    }
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <ul>
+        <li className={styles.addBoard} onClick={openModal}>
+          <div className={styles.addBoardPlus}>
+            <div className={styles.gorizontalLine}></div>
+            <div className={styles.verticalLine}></div>
+          </div>
+        </li>
+      </ul>
+      {renderCreateBoardModal()}
+    </div>
+  );
 };
 
 export default Boards;
