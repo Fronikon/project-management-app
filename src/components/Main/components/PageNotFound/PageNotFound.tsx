18d00c9c3c736754200381287fa5ec74250bc9a5
Andrey Lavryonov <kedr_club@mail.ru>
2022-11-16 08:16:03 +0300

    feat: add change language to page pageNotFound

--- src/components/Main/components/PageNotFound/PageNotFound.tsx
+++ src/components/Main/components/PageNotFound/PageNotFound.tsx
@@ -1,16 +1,15 @@
 import React, { FC } from 'react';
 import styles from './PageNotFound.module.css';
 import fone from '../../../../assets/img/other/404.png';
-
-const title = {
-  ru: 'К сожалению такой страницы не существует',
-  en: 'Sorry, there is no such page',
-};
+import textData from '../../../../data/textData';
+import { useAppSelector } from '../../../../hooks/reduxHooks';
 
 const PageNotFound: FC = () => {
+  const language = useAppSelector((store) => store.language.value);
+
   return (
     <div className={styles.wrapper}>
-      <h2 className={styles.title}>{title.ru}</h2>
+      <h2 className={styles.title}>{textData.pageNotFound[language]}</h2>
       <img className={styles.image} src={fone} alt="Error 404." />
     </div>
   );
