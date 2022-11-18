f870610aa025a4059f653d9d573866b984c3db5e
Andrey Lavryonov <kedr_club@mail.ru>
2022-11-18 22:52:29 +0300

    fix: small changes in the footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -12,17 +12,23 @@ const Footer: FC = () => {
       <a href="https://rs.school/react/" className={styles.logoLink}>
         <img src={rss} alt="RssLogo" className={styles.logo} />
       </a>
-      <div className={styles.github}>
-        <a href="https://github.com/Fronikon" className={styles.githubLink}>
-          {textData.footer.Dmitry[language]}
-        </a>
-        <a href="https://github.com/InnokentyKedrov" className={styles.githubLink}>
-          {textData.footer.Andrey[language]}
-        </a>
-        <a href="https://github.com/karap9s" className={styles.githubLink}>
-          {textData.footer.Daniil[language]}
-        </a>
-      </div>
+      <ul className={styles.github}>
+        <li>
+          <a href="https://github.com/Fronikon" className={styles.githubLink}>
+            {textData.footer.Dmitry[language]}
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/InnokentyKedrov" className={styles.githubLink}>
+            {textData.footer.Andrey[language]}
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/karap9s" className={styles.githubLink}>
+            {textData.footer.Daniil[language]}
+          </a>
+        </li>
+      </ul>
       <p className={styles.year}>© 2022</p>
     </footer>
   );
