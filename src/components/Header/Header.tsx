41ea3bb426932435abd880bc7f0a67168e9b8bf8
Andrey Lavryonov <kedr_club@mail.ru>
2022-11-27 20:01:43 +0300

    feat: add modal window for log out

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import styles from './Header.module.css';
 import { switchEng, switchRu } from '../../store/reducers/languageReducer';
 import { useAppDispatch, useAppSelector } from '../../hooks/reduxHooks';
@@ -10,11 +10,14 @@ import edit from '../../assets/img/icons/edit.png';
 import exit from '../../assets/img/icons/exit.png';
 import { NavLink } from 'react-router-dom';
 import { logOut } from '../../store/slices/sliceAuth';
+import Modal from '../../componentsUtils/Modal/Modal';
+import ConfirmAction from '../../componentsUtils/forms/ConfirmActionForm/ConfirmActionForm';
 
 const Header: FC = () => {
   const language = useAppSelector((store) => store.language.value);
   const token = useAppSelector((store) => store.authReducer.token);
   const dispatch = useAppDispatch();
+  const [isModal, setIsModal] = useState(false);
 
   const switchCheck = () => {
     if (language === 'eng') {
@@ -24,64 +27,88 @@ const Header: FC = () => {
     }
   };
 
+  const closeModal = () => {
+    setIsModal(false);
+  };
+
   const onClick = () => {
+    setIsModal(true);
+  };
+
+  const confirm = () => {
+    setIsModal(false);
     localStorage.removeItem('token');
     dispatch(logOut());
   };
 
+  const cancel = () => {
+    setIsModal(false);
+  };
+
   return (
-    <header className={styles.headerWrapper}>
-      <div className={styles.leftBlock}>
-        <h1 className={styles.heading}>Doska</h1>
-        <NavLink className={styles.home} to="/" end>
-          <img src={home} alt="home" className={styles.homeImage} />
-          <p className={styles.homeText}>{textData.header.home[language]}</p>
-        </NavLink>
-      </div>
-      <div className={styles.rightBlock}>
-        <div className={styles.language}>
-          <label className={styles.switch}>
-            <input type="checkbox" onClick={switchCheck} />
-            <span
-              className={`${styles.slider} ${language === 'eng' && styles.sliderEng} ${
-                language === 'ru' && styles.sliderRu
-              }  ${styles.round}`}
-            ></span>
-          </label>
-          <div className={styles.languageText}>
-            <p className={`${styles.languageRus} ${language === 'ru' && styles.activeLanguage}`}>
-              Русский
-            </p>
-            <p className={`${styles.languageEng} ${language === 'eng' && styles.activeLanguage}`}>
-              English
-            </p>
-          </div>
+    <>
+      {isModal && (
+        <Modal closeModal={closeModal}>
+          <ConfirmAction
+            // question={textData.boardsPage.questionConfirmingDeleteBoard[language]}
+            confirm={confirm}
+            cancel={cancel}
+          />
+        </Modal>
+      )}
+      <header className={styles.headerWrapper}>
+        <div className={styles.leftBlock}>
+          <h1 className={styles.heading}>Doska</h1>
+          <NavLink className={styles.home} to="/" end>
+            <img src={home} alt="home" className={styles.homeImage} />
+            <p className={styles.homeText}>{textData.header.home[language]}</p>
+          </NavLink>
         </div>
-        {token ? (
-          <div className={styles.auth}>
-            <NavLink className={styles.signIn} to="/edit" end>
-              <img src={edit} alt="Edit profile." className={styles.signInImage} />
-              <p className={styles.signInText}>{textData.header.edit[language]}</p>
-            </NavLink>
-            <div className={styles.signUp} onClick={onClick}>
-              <img src={exit} alt="Exit." className={styles.signUpImage} />
-              <p className={styles.signUpText}>{textData.header.exit[language]}</p>
+        <div className={styles.rightBlock}>
+          <div className={styles.language}>
+            <label className={styles.switch}>
+              <input type="checkbox" onClick={switchCheck} />
+              <span
+                className={`${styles.slider} ${language === 'eng' && styles.sliderEng} ${
+                  language === 'ru' && styles.sliderRu
+                }  ${styles.round}`}
+              ></span>
+            </label>
+            <div className={styles.languageText}>
+              <p className={`${styles.languageRus} ${language === 'ru' && styles.activeLanguage}`}>
+                Русский
+              </p>
+              <p className={`${styles.languageEng} ${language === 'eng' && styles.activeLanguage}`}>
+                English
+              </p>
             </div>
           </div>
-        ) : (
-          <div className={styles.auth}>
-            <NavLink className={styles.signIn} to="/signIn" end>
-              <img src={signIn} alt="Sign In." className={styles.signInImage} />
-              <p className={styles.signInText}>{textData.header.signIn[language]}</p>
-            </NavLink>
-            <NavLink className={styles.signUp} to="/signUp" end>
-              <img src={signUp} alt="Sign Up." className={styles.signUpImage} />
-              <p className={styles.signUpText}>{textData.header.signUp[language]}</p>
-            </NavLink>
-          </div>
-        )}
-      </div>
-    </header>
+          {token ? (
+            <div className={styles.auth}>
+              <NavLink className={styles.signIn} to="/edit" end>
+                <img src={edit} alt="Edit profile." className={styles.signInImage} />
+                <p className={styles.signInText}>{textData.header.edit[language]}</p>
+              </NavLink>
+              <div className={styles.signUp} onClick={onClick}>
+                <img src={exit} alt="Exit." className={styles.signUpImage} />
+                <p className={styles.signUpText}>{textData.header.exit[language]}</p>
+              </div>
+            </div>
+          ) : (
+            <div className={styles.auth}>
+              <NavLink className={styles.signIn} to="/signIn" end>
+                <img src={signIn} alt="Sign In." className={styles.signInImage} />
+                <p className={styles.signInText}>{textData.header.signIn[language]}</p>
+              </NavLink>
+              <NavLink className={styles.signUp} to="/signUp" end>
+                <img src={signUp} alt="Sign Up." className={styles.signUpImage} />
+                <p className={styles.signUpText}>{textData.header.signUp[language]}</p>
+              </NavLink>
+            </div>
+          )}
+        </div>
+      </header>
+    </>
   );
 };
 
