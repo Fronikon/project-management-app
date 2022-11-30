874cf5418db25eb0673c8a7ae901bc17e70eb5cd
Dmitry Beresnev <dmitrii.beresnev@automate-business.ru>
2022-12-01 00:13:35 +0300

    feat: add lazy components

--- src/components/Main/components/index.ts
+++ src/components/Main/components/index.ts
@@ -1,8 +1,10 @@
-import Boards from './Boards/Boards';
-import PageNotFound from './PageNotFound/PageNotFound';
-import SignIn from './SignIn/SignIn';
-import SignUp from './SignUp/SignUp';
-import Welcome from './Welcome/Welcome';
-import Board from './Board/Board';
+import { lazy } from 'react';
+
+const Boards = lazy(() => import('./Boards/Boards'));
+const PageNotFound = lazy(() => import('./PageNotFound/PageNotFound'));
+const SignIn = lazy(() => import('./SignIn/SignIn'));
+const SignUp = lazy(() => import('./SignUp/SignUp'));
+const Welcome = lazy(() => import('./Welcome/Welcome'));
+const Board = lazy(() => import('./Board/Board'));
 
 export { Boards, Board, PageNotFound, SignIn, SignUp, Welcome };
